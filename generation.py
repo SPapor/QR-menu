492ee822ea372caf6b29d4c066b282baef5a611e
SH21 <anosovigor404@gmail.com>
2025-02-03 15:24:14 +0200

    KAN-1: feat:Надеюсь финал

--- generation.py
+++ generation.py
@@ -1,10 +1,9 @@
 from typing import Any
 
 import qrcode
-from datetime import datetime
 
-timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-def generator_qrcode(link_id:Any):
+
+def generator_qrcode(link_id: Any):
     qr = qrcode.QRCode(
         version=5,
         error_correction=qrcode.constants.ERROR_CORRECT_M,
@@ -14,5 +13,4 @@ def generator_qrcode(link_id:Any):
     qr.add_data(link_id)
     qr.make(fit=True)
     img = qr.make_image(fill="black", back_color="white")
-    return img.save(f"{timestamp}.png")
-generator_qrcode("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    return img
