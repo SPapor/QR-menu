4602e3616012639040775d70252f6c732db31b23
spapor <kekuhaleksandr@gmail.com>
2025-02-12 00:52:00 +0200

    chore:add more test

--- tests/test_user/test_user_repo.py
+++ tests/test_user/test_user_repo.py
@@ -1,7 +1,9 @@
-from user.models import User
 from uuid import UUID
 
-from test_user.conftest import user
+import pytest
+
+from app.user.models import User
+
 
 async def test_user_repo_get_by_id(user_repo, user_dto_in_db):
     u = await user_repo.get_by_id(user_dto_in_db["id"])
@@ -9,6 +11,13 @@ async def test_user_repo_get_by_id(user_repo, user_dto_in_db):
     assert u.id == user_dto_in_db["id"]
     assert u.username == user_dto_in_db["username"]
 
-async def test_user_crud_create(user_crud, user_dto):
-    id_ = await user_crud.create(user_dto)
+
+async def test_user_repo_create(user_repo, user):
+    id_ = await user_repo.create(user)
     assert isinstance(id_, UUID)
+
+
+@pytest.mark.parametrize("users_number", [2])
+async def test_user_repo_create_many(user_repo, users, users_number):
+    ids = await user_repo.create_many(users)
+    assert len(ids) == len(users)
