16f4a61d964dc9990f5fd03cd8e997c2feef4117
spapor <kekuhaleksandr@gmail.com>
2025-02-16 16:15:15 +0200

    refactor: get_image

--- app/qr_code/models.py
+++ app/qr_code/models.py
@@ -1,7 +1,8 @@
 import uuid
 from dataclasses import dataclass, field
 from uuid import UUID
-
+import qrcode
+from PIL import Image
 
 @dataclass(kw_only=True)
 class QrCode:
@@ -9,3 +10,15 @@ class QrCode:
     user_id: UUID
     name: str
     link: str
+
+    def get_image(self) -> Image.Image:
+        qr = qrcode.main.QRCode(
+            version=5,
+            error_correction=qrcode.constants.ERROR_CORRECT_M,
+            box_size=10,
+            border=4,
+        )
+        qr.add_data(self.link)
+        qr.make(fit=True)
+        img = qr.make_image(fill="black", back_color="white")
+        return img
